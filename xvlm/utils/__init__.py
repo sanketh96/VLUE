034cf3976716084b4431824461734c749648e046
Sanketh Rangreji <sankethrangreji@sankeths-mbp-2.wifi.local.cmu.edu>
2023-03-03 22:29:46 -0500

    Working changes

--- xvlm/utils/__init__.py
+++ xvlm/utils/__init__.py
@@ -335,6 +335,9 @@ def save_on_master(*args, **kwargs):
 
 
 def init_distributed_mode(args):
+    print('Not using distributed mode')
+    args.distributed = False
+    return
     if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
         args.rank = int(os.environ["RANK"])
         args.world_size = int(os.environ['WORLD_SIZE'])
